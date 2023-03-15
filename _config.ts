8c2ff80df750fe1e71be8187265f11b99219729f
Oscar Otero <oom@oscarotero.com>
2023-03-15 18:00:09 +0100

    progress

--- _config.ts
+++ _config.ts
@@ -2,8 +2,12 @@ import lume from "lume/mod.ts";
 import postcss from "lume/plugins/postcss.ts";
 import esbuild from "lume/plugins/esbuild.ts";
 import inline from "lume/plugins/inline.ts";
+import multilanguage from "lume/plugins/multilanguage.ts";
+import date from "lume/plugins/date.ts";
 import postcssExtendRule from "npm:postcss-extend-rule@4.0.0";
 import postcssCustomMedia from "npm:postcss-custom-media@9.1.2";
+import gl from "npm:date-fns/locale/gl/index.js";
+import es from "npm:date-fns/locale/es/index.js";
 
 const site = lume(
   {},
@@ -13,6 +17,9 @@ const site = lume(
 site.copy("fonts");
 site.copy("img");
 site.use(inline());
+site.use(date({
+  locales: { gl, es }
+}));
 
 site.use(postcss({
   plugins: [
@@ -23,5 +30,6 @@ site.use(postcss({
 }));
 
 site.use(esbuild());
+site.use(multilanguage());
 
 export default site;
