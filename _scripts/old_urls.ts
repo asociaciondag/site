cf1737b09e35c77e127c63b3fea863766a3a0529
Oscar Otero <oom@oscarotero.com>
2025-09-06 15:50:37 +0200

    deno fmt

--- _scripts/old_urls.ts
+++ _scripts/old_urls.ts
@@ -1,13 +1,13 @@
 import { expandGlob } from "jsr:@std/fs/expand-glob";
-import { parse } from "jsr:@std/yaml/parse"
-import { stringify } from "jsr:@std/yaml/stringify"
+import { parse } from "jsr:@std/yaml/parse";
+import { stringify } from "jsr:@std/yaml/stringify";
 
 for await (const file of expandGlob("./portfolio/*.yml")) {
   if (file.name.startsWith("_")) continue; // Skip files starting with underscore
-  
+
   const content = await Deno.readTextFile(file.path);
   const data = parse(content) as any;
-  const {lang, basename } = data;
+  const { lang, basename } = data;
 
   if (basename && lang) {
     if (lang === "gl") {
--- _scripts/rename.ts
+++ _scripts/rename.ts
@@ -1,12 +1,12 @@
 import { expandGlob } from "jsr:@std/fs/expand-glob";
-import { parse } from "jsr:@std/yaml/parse"
+import { parse } from "jsr:@std/yaml/parse";
 
 for await (const file of expandGlob("./pages/*.yml")) {
   if (file.name.startsWith("_")) continue; // Skip files starting with underscore
-  
+
   const content = await Deno.readTextFile(file.path);
   const data = parse(content) as any;
-  const {id, lang, basename } = data;
+  const { id, lang, basename } = data;
 
   if (id && lang) {
     const langSuffix = Array.isArray(lang) ? "" : `_${lang}`;
