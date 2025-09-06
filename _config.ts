f3d0484233aa58be48f01563c40fd6f34c4d93e5
Oscar Otero <oom@oscarotero.com>
2025-09-06 16:48:03 +0200

    removed css and clean HTML

--- _config.ts
+++ _config.ts
@@ -1,13 +1,11 @@
 import lume from "lume/mod.ts";
-import postcss from "lume/plugins/postcss.ts";
+import lightningcss from "lume/plugins/lightningcss.ts";
 import esbuild from "lume/plugins/esbuild.ts";
 import inline from "lume/plugins/inline.ts";
 import multilanguage from "lume/plugins/multilanguage.ts";
 import relations from "lume/plugins/relations.ts";
 import date from "lume/plugins/date.ts";
 import slugifyUrls from "lume/plugins/slugify_urls.ts";
-import postcssExtendRule from "npm:postcss-extend-rule@4.0.0";
-import postcssCustomMedia from "npm:postcss-custom-media@9.1.2";
 import basePath from "lume/plugins/base_path.ts";
 import gl from "npm:date-fns/locale/gl/index.js";
 import es from "npm:date-fns/locale/es/index.js";
@@ -23,12 +21,7 @@ site.add("fonts")
   .use(esbuild())
   .add("css")
   .add("js")
-  .use(postcss({
-    plugins: [
-      postcssExtendRule(),
-      postcssCustomMedia,
-    ],
-  }))
+  .use(lightningcss())
   .use(slugifyUrls())
   .use(inline())
   .use(basePath())
