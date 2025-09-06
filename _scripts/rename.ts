bd9c4917eb071592e2fb1c1768e1a4a1c5621111
Oscar Otero <oom@oscarotero.com>
2025-09-06 15:29:29 +0200

    renomeados os arquivos co contido da web

--- _scripts/rename.ts
+++ _scripts/rename.ts
@@ -6,10 +6,15 @@ for await (const file of expandGlob("./news/*.yml")) {
   
   const content = await Deno.readTextFile(file.path);
   const data = parse(content) as any;
-  const {id, lang} = data;
+  const {id, lang, basename } = data;
 
   if (id && lang) {
-    const newName = `${id}_${file.name.replace(/\.yml$/, `_${lang}.yml`)}`;
+    const langSuffix = Array.isArray(lang) ? "" : `_${lang}`;
+    let name = basename;
+    if (name.endsWith(`-${lang}`)) {
+      name = name.slice(0, -(`-${lang}`).length);
+    }
+    const newName = `${id}_${name}${langSuffix}.yml`;
     Deno.rename(file.path, `./news/${newName}`);
   }
 }
