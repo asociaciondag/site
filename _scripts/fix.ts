acab24634d9d0bedfa61756cf2bc8e7cd0ccd73d
Oscar Otero <oom@oscarotero.com>
2025-09-14 16:36:15 +0200

    axustes portfolios

--- _scripts/fix.ts
+++ _scripts/fix.ts
@@ -2,62 +2,48 @@ import { expandGlob } from "jsr:@std/fs/expand-glob";
 import { parse } from "jsr:@std/yaml/parse";
 import { stringify } from "jsr:@std/yaml/stringify";
 
-for await (const file of expandGlob("./socios/*.yml")) {
+for await (const file of expandGlob("./portfolio/*.yml")) {
   if (file.name.startsWith("_")) continue; // Skip files starting with underscore
 
   const content = await Deno.readTextFile(file.path);
   const data = parse(content) as any;
-  const { links } = data;
+  const { info } = data;
 
-  for (const link of links) {
-    if (link.text === "Behance") {
-      delete link.text;
-      link.type = "behance";
+  if (!info) continue;
+
+  for (const item of Object.values(info) as any[]) {
+    item.name = item.name.endsWith(":") ? item.name.slice(0, -1) : item.name;
+    item.name = item.name.trim();
+  }
+
+  const year = info.find((item: any) =>
+    item.name === "Ano" || item.name === "Año"
+  );
+  if (year) {
+    data.year = year.value;
+    info.splice(info.indexOf(year), 1);
+  }
+  const client = info.find((item: any) => item.name === "Cliente");
+  if (client) {
+    data.client = client.value;
+    info.splice(info.indexOf(client), 1);
+  }
+  const studio = info.find((item: any) => item.name === "Estudio");
+  if (studio) {
+    data.studio = studio.value;
+    info.splice(info.indexOf(studio), 1);
+  }
+  const project = info.find((item: any) =>
+    item.name === "Proxecto" || item.name === "Proyecto"
+  );
+  if (project) {
+    if (data.title === project.value) {
+      info.splice(info.indexOf(project), 1);
     }
-    if (link.text === "Instagram") {
-      delete link.text;
-      link.type = "instagram";
-    }
-    if (link.text === "Facebook") {
-      delete link.text;
-      link.type = "facebook";
-    }
-    if (link.text === "Threads") {
-      delete link.text;
-      link.type = "threads";
-    }
-    if (link.text === "Youtube") {
-      delete link.text;
-      link.type = "youtube";
-    }
-    if (link.text === "Pinterest") {
-      delete link.text;
-      link.type = "pinterest";
-    }
-    if (link.text === "Github") {
-      delete link.text;
-      link.type = "github";
-    }
-    if (link.text?.trim() === "Linkedin") {
-      delete link.text;
-      link.type = "linkedin";
-    }
-    if (link.text?.trim() === "Spotify") {
-      delete link.text;
-      link.type = "spotify";
-    }
-    if (link.text === "Tumblr") {
-      delete link.text;
-      link.type = "tumblr";
-    }
-    if (link.type === "linkedin" || link.type === "facebook") {
-      delete link.text;
-    }
-    if (link.text === "X" || link.text?.trim() === "Twitter") {
-      delete link.text;
-      link.url = link.url.replace("twitter.com", "x.com");
-      link.type = "x";
-    }
+  }
+
+  if (info.length === 0) {
+    delete data.info;
   }
 
   Deno.writeTextFileSync(file.path, stringify(data));
