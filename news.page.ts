84c787f3d2913e8a76ad33c251034f216a923cf1
Oscar Otero <oom@oscarotero.com>
2025-09-15 20:57:19 +0200

    Novas

--- news.page.ts
+++ news.page.ts
@@ -3,9 +3,17 @@ export const lang = ["gl", "es"];
 
 export const gl = {
   title: "Novas",
+  description: `
+  Podes suscribirte ao noso [feed RSS](#) para estar ao tanto das últimas novas.
+  Tamén podes seguirnos nas redes sociais de [X](https://x.com/asociaciondag) e [Instagram](https://www.instagram.com/asociaciondag).
+  `,
 };
 export const es = {
   title: "Noticias",
+  description: `
+  Puedes suscribirte a nuestro [feed RSS](#) para estar al tanto de las últimas noticias.
+  También puedes seguirnos en las redes sociales de [X](https://x.com/asociaciondag) e [Instagram](https://www.instagram.com/asociaciondag).
+  `,
 };
 
 export default function* ({ search, paginate, lang }: Lume.Data) {
